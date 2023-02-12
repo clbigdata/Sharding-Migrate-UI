88af67f59fd9377da50e58f3d70ce8332bf9c328
chenlong <clbigdata@163.com>
2023-02-12 11:54:43 +0800

    响应式案例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,35 +1,20 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { defineComponent, reactive } from 'vue'
+interface CountObject{
+  count:number
+}
 
-defineProps<{ msg: string }>()
+const reactiveObj = reactive<CountObject>({count:0})
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-  <p>这是一个开源项目</p>
 
   <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    响应式：Count:{{ reactiveObj.count }}
+    <span><button @click="reactiveObj.count++">加1</button></span>
   </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  
 </template>
 
 <style scoped>
